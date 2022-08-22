f90601e8dacd2734ed70f0decc120f72c199f99e
lucapetcu <lucapetcu@yahoo.com>
2022-08-22 18:24:42 +0300

    Updated frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { NgForm } from '@angular/forms';
 import { Marker } from './utils/marker';
 import { CoordResponse } from './utils/coordResponse';
 import { UserResponse } from './utils/userResponse';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ import { UserResponse } from './utils/userResponse';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  @ViewChild('map') map!: google.maps.Map;
+  // @ViewChild('map') map!: google.maps.Map;
+  @ViewChild(GoogleMap) map!: GoogleMap;
   @ViewChild('f') form!: NgForm;
   
   center!: google.maps.LatLngLiteral;
@@ -24,20 +26,37 @@ export class AppComponent implements OnInit{
   showAllCoordinatesAfter: boolean = false;
   showDatePicker: boolean = false;
   showLiveUpdates: boolean = false;
+  trackUser: boolean = false;
   
   noData: boolean = false;
   noAvailableData: boolean = false;
-  noUsernameSelected: boolean = false;
+  noUsernameSelected: boolean = true;
 
   lastLocation!: Marker;
   markers: Marker[] = [];
   liveMarkers: Marker[] = [];
+  geofenceMarkers: Marker[] = [];
   polylineOptions = {
     path:  [],
     strokeColor: "#FF0000",
     strokeOpacity: 1.0,
     strokeWeight: 2
   };
+
+  polygonOptions = {
+    paths: [],
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.5
+  };  
+
+  vertices: google.maps.LatLngLiteral[] = [
+    {lat: 13, lng: 13},
+    {lat: -13, lng: 0},
+    {lat: 13, lng: -13},
+  ];
 
   selectedUserName: string = '';
   users: { _id: string, user_email: string, user_name: string }[] = [];
@@ -62,11 +81,15 @@ export class AppComponent implements OnInit{
   }
 
   onShowLastLocation() {
-    this.http
+    if (!this.selectedUserName) {
+      alert('You must select an username first!');
+    } else {
+      this.http
       .get<CoordResponse>('http://127.0.0.1:3000/coordinates/get-last-coordinates/' + this.selectedUserName)
       .subscribe(resData => {
         if (resData.locations.length === 0) {
           this.noAvailableData = true;
+          alert(`No available data for ${this.selectedUserName}`);
         } else {
           this.noAvailableData = false;
           this.lastLocation = {position: {lat: resData.locations[0].latitude, lng: resData.locations[0].longitude}};
@@ -74,6 +97,7 @@ export class AppComponent implements OnInit{
           this.showLastCoordinates = true;
         }
       });
+    }
   }
 
   onRemoveLastLocation() {
@@ -81,7 +105,10 @@ export class AppComponent implements OnInit{
   }
 
   onShowAllLocations() {
-    this.http
+    if (!this.selectedUserName) {
+      alert('You must select an username first!');
+    } else {
+      this.http
       .get<CoordResponse>('http://127.0.0.1:3000/coordinates/get-coordinates/user-name/' + this.selectedUserName)
       .subscribe(resData => {
         this.noData = false;
@@ -105,6 +132,7 @@ export class AppComponent implements OnInit{
         }
         this.showAllCoordinates = true;
       });
+    }
   }
 
   onRemoveAllLocations() {
@@ -148,12 +176,13 @@ export class AppComponent implements OnInit{
     this.liveMarkers.splice(0, this.liveMarkers.length);
     if (this.selectedUserName === '') {
       this.noUsernameSelected = true;
+      alert('You must select an username first!');
     } else {
       this.interval = setInterval(() => {
         this.http
           .get<CoordResponse>('http://127.0.0.1:3000/coordinates/get-last-coordinates/' + this.selectedUserName)
           .subscribe(resData => {
-            console.log(resData);
+            // console.log(resData);
             if (resData.locations.length === 0) {
               this.noAvailableData = true;
             } else {
@@ -161,12 +190,35 @@ export class AppComponent implements OnInit{
                 this.liveMarkers.push({
                   position: {lat: resData.locations[0].latitude, lng: resData.locations[0].longitude}
                 });
+                if (this.trackUser) {
+                  const path = [];
+                  for (let i = 0; i < this.geofenceMarkers.length; i++) {
+                    path.push({
+                      lat: this.geofenceMarkers[i].position.lat,
+                      lng: this.geofenceMarkers[i].position.lng
+                    });
+                  }
+                  const polygon = new google.maps.Polygon({paths: path});
+                  const result = google.maps.geometry.poly.containsLocation(new google.maps.LatLng(resData.locations[0].latitude, resData.locations[0].longitude), polygon);
+                  console.log(result);
+                }
               } else if (this,this.liveMarkers.length === 0) {
                 this.liveMarkers.push({
                   position: {lat: resData.locations[0].latitude, lng: resData.locations[0].longitude}
                 });
+                if (this.trackUser) {
+                  const path = [];
+                  for (let i = 0; i < this.geofenceMarkers.length; i++) {
+                    path.push({
+                      lat: this.geofenceMarkers[i].position.lat,
+                      lng: this.geofenceMarkers[i].position.lng
+                    });
+                  }
+                  const polygon = new google.maps.Polygon({paths: path});
+                  const result = google.maps.geometry.poly.containsLocation(new google.maps.LatLng(resData.locations[0].latitude, resData.locations[0].longitude), polygon);
+                  console.log(result);
+                }
               }
-              
               this.showLiveUpdates = true;
             }
           });
@@ -192,13 +244,41 @@ export class AppComponent implements OnInit{
   onChangedStartService(event: any) {
     console.log(event.currentTarget.checked);
     if (event.currentTarget.checked) {
-      this.http
-        .post('http://127.0.0.1:3000/settings/start-service', {user_name: this.selectedUserName, start: 'start'})
-        .subscribe(resData => console.log(resData));
+      if (!this.selectedUserName) {
+        alert('No username was selected. Please select one before starting the service!');
+        event.currentTarget.checked = false;
+      } else {
+        this.http
+          .post('http://127.0.0.1:3000/settings/start-service', {user_name: this.selectedUserName, start: 'start'})
+          .subscribe(resData => console.log(resData));
+      }
     } else {
       this.http
         .post('http://127.0.0.1:3000/settings/start-service', {user_name: this.selectedUserName, start: 'stop'})
         .subscribe(resData => console.log(resData));
     }
+  }
+
+  onMapClicked(event: google.maps.MapMouseEvent) {
+    this.geofenceMarkers.push({
+      position: {
+        lat: event.latLng?.lat() as number,
+        lng: event.latLng?.lng() as number
+      }
+    });
+  }
+
+  onStartTracking() {
+    if (this.geofenceMarkers.length === 0) {
+      alert('You must select at least 3 points on the map, then click again to start tracking');
+    } else if (!this.showLiveUpdates){
+      alert('You must enable live updates for the userm then click again to start tracking');
+    } else {
+      this.trackUser = true;
+    }
+  }
+
+  onStopTracking() {
+    this.trackUser = false;
   }
 }
